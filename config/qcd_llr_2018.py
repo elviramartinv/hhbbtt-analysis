a4b1b7dcdd81ddf9216e9ea4e782dde81412d961
Elvira <elvira.martin@ciemat.es>
2024-02-21 10:19:08 +0100

    rm processes

--- config/qcd_llr_2018.py
+++ config/qcd_llr_2018.py
@@ -3,29 +3,50 @@ from analysis_tools.utils import DotDict
 from analysis_tools.utils import join_root_selection as jrs
 from plotting_tools import Label
 from collections import OrderedDict
+import os
 
 from config.llr_2018 import Config as base_config
 
 class Config(base_config):
-    def add_processes(self):
-        v9_processes = super(Config, self).add_processes()
-        processes = [
-            Process("ggf_s0", Label("HH_{ggF}"), color=(0, 0, 0), isSignal=True),
-            Process("ggf_s2", Label("HH_{ggF}"), color=(0, 0, 0), isSignal=True)
-        ]
-        processes = ObjectCollection(processes)
+    # def add_processes(self):
+    #     processes = [
+    #         Process("ggf_s0", Label("HH_{ggF}"), color=(0, 0, 0), isSignal=True),
+    #         Process("ggf_s2", Label("HH_{ggF}"), color=(0, 0, 0), isSignal=True)
+    #     ]
+    #     processes_v9 = super(Config, self).add_processes()
+    #     process_group_names = {
+    #         "default": [
+    #             # "ggf_sm",
+    #             # "data_tau",
+    #             # "dy_high",
+    #             # "tt_dl",
+    #             # "data",
+    #             "dy",
+    #             "tt",
+    #             "others"
+    #         ]
+    #     }
+    #     process_training_names = {
+    #         "default": [
+    #             "ggf_sm",
+    #             "dy"
+    #         ]
+    #     }
+    #     # process_group_names = super(Config, self).add_process_group_names()
+    #     # process_training_names = super(Config, self).add_process_training_names()   
+    #     return ObjectCollection(processes + processes_v9), process_group_names, process_training_names
         
     def add_datasets(self):
         skim_directory = "/eos/user/l/lportale/hhbbtautau/skims/SKIMS_UL18/"
         datasets = [
             Dataset("ggf_s0_m250",
                 folder=os.path.join(skim_directory, "SKIM_ggF_Radion_m250"),
-                process=self.processes.get("ggf_s0"),
+                process=self.processes.get("ggf"),
                 file_pattern="output_.*root",
                 xs=1.),
             Dataset("ggf_s2_m250",
                 folder=os.path.join(skim_directory, "SKIM_ggF_BulkGraviton_m250"),
-                process=self.processes.get("ggf_s2"),
+                process=self.processes.get("ggf"),
                 file_pattern="output_.*root",
                 xs=1.),
             Dataset("tt_dl",
@@ -47,8 +68,9 @@ class Config(base_config):
                 folder=os.path.join(skim_directory, "SKIM_DY_amc_incl"),
                 process=self.processes.get("dy"),
                 file_pattern="output_.*root",
-                xs=1.),
+                xs=1.,
+                tags=["ul"]),
         ]
-        datasets = ObjectCollection(datasets)
+        return ObjectCollection(datasets)
 
 config = Config("qcd_llr_2018", year=2018, ecm=13, lumi_pb=59741)
